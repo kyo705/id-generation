5382094c9daa6ac566b43e9d6556cdb398cd1182
kyo705 <mission0705@naver.com>
2023-10-26 17:04:36 +0900

    [ADD] id generation test
    1. concurrency test
    2. required cps(creationCnt per second) test

--- src/test/java/org/idgeneration/IdGenerationApplicationTests.java
+++ src/test/java/org/idgeneration/IdGenerationApplicationTests.java
@@ -1,13 +1,53 @@
 package org.idgeneration;
 
+import org.assertj.core.api.Assertions;
+import org.idgeneration.idgenerator.IdGenerator;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.HashSet;
+import java.util.Set;
+
+
 class IdGenerationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testConcurrency() {
+
+		// given
+		Set<Long> idSet = new HashSet<>();
+
+		IdGenerator idGenerator = new IdGenerator(1, System.currentTimeMillis());
+
+		// when
+		Runnable backgroundThread = () -> {
+			for(int i=0;i<100000;i++){
+				idSet.add(idGenerator.generateId());
+			}
+		};
+		backgroundThread.run();
+
+		for(int i=0;i<100000;i++){
+			idSet.add(idGenerator.generateId());
+		}
+
+		Assertions.assertThat(idSet.size()).isEqualTo(200000);
 	}
 
+	@Test
+	void testCreationCntPerSecond() {
+
+		// given
+		long requiredCreationCntPerSecond = 10000;
+		long second = 1000;
+		IdGenerator idGenerator = new IdGenerator(1, System.currentTimeMillis());
+
+		long startTimestamp = System.currentTimeMillis();
+		long cnt = 0L;
+		while(System.currentTimeMillis() - startTimestamp < second) {
+			idGenerator.generateId();
+			cnt++;
+		}
+		Assertions.assertThat(cnt).isGreaterThan(requiredCreationCntPerSecond);
+		System.out.println("초당 id 생성 횟수 : " + cnt + "개");
+	}
 }
